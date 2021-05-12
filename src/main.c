752fc431f8ac468aaebe488dc531b73c61e49740
jjzn <50965831+jjzn@users.noreply.github.com>
2021-05-12 12:54:47 +0200

    Open the input file

--- src/main.c
+++ src/main.c
@@ -28,15 +28,24 @@ int main(int argc, char** argv) {
     }
 
     char* name = argv[1];
-    char* out = replace_suffix(name, ".img");
+    char* output = replace_suffix(name, ".img");
 
-    if (out == NULL) {
+    if (output == NULL) {
         fputs("fatal: memory allocation error, aborting...\n", stderr);
         return 1;
     }
 
-    debug("assembling %s --> %s\n", name, out);
+    debug("assembling %s --> %s\n", name, output);
 
-    free(out);
+    FILE* in = fopen(name, "r");
+    if (in == NULL) {
+        perror(name);
+        free(output);
+        return 1;
+    }
+
+
+    fclose(in);
+    free(output);
     return 0;
 }
