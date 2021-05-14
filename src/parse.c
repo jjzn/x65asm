a456f5b38af960fc1f9359c8ede1003d9004c033
jjzn <50965831+jjzn@users.noreply.github.com>
2021-05-14 11:08:25 +0200

    Fix some compiler warnings/errors

--- src/parse.c
+++ src/parse.c
@@ -1,7 +1,12 @@
+#include <stdlib.h>
+
 #include "defs.h"
 
 line_t parse(char* buff) {
     /* TODO: implement */
+
+    if (buff) {} /* stop the compiler complaining */
+
     return (line_t) {
         NULL, PSEUDO, NULL, NULL
     };
