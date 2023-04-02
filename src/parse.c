8f453ca7b7921f38b4adae1ccb1a5ccc11bfd277
jjzn <jjzn@protonmail.com>
2023-04-02 13:39:20 +0200

    Use allocated line_t members
    Member elements of line_t are now allocated and tokens are copied into them

--- src/parse.c
+++ src/parse.c
@@ -7,20 +7,37 @@
 line_t parse(char* buff) {
     /* TODO: add support for comments */
 
-    char *label, *op, *arg;
-    label = op = arg = NULL;
+    char* label = calloc(1024, sizeof(char));
+    if (label == NULL)
+        debug("parse: error: could not allocate memory\n"); /* TODO: should be a panic */
+
+    char* op = calloc(1024, sizeof(char));
+    if (op == NULL)
+        debug("parse: error: could not allocate memory\n"); /* TODO: should be a panic */
+
+    char* arg = calloc(1024, sizeof(char));
+    if (arg == NULL)
+        debug("parse: error: could not allocate memory\n"); /* TODO: should be a panic */
+
     line_type_t type = -1;
 
     char* tk = strtok(buff, " \t");
     while (tk != NULL) {
         size_t len = strlen(tk);
         if (tk[len - 1] == ':') {
-            if (label == NULL)
-                label = tk;
+            if (*label == '\0')
+                strncpy(label, tk, 1024);
             else
                 debug("parse: error: too many labels in this line\n"); /* TODO: should be a panic */
 
-        } else if (*tk == '.') {
+        } else if (*op == '\0')
+            strncpy(op, tk, 1024);
+        else if (*arg == '\0')
+            strncpy(arg, tk, 1024);
+        else
+            debug("parse: error: too many arguments in this line\n"); /* TODO: should be a panic */
+
+        if (*tk == '.') {
             if (type == (line_type_t) -1)
                 type = PSEUDO;
             else
