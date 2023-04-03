5074a73be19de1246c0abf9f0081857ed4fc84ed
jjzn <jjzn@protonmail.com>
2023-04-03 10:26:11 +0200

    Add instruction mapping table and more boilerplate

--- src/emit.c
+++ src/emit.c
@@ -1,12 +1,33 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "defs.h"
 #include "maybe.h"
+
+typedef emit_t (*emitter)(line_t);
+
+emit_t emit_lda(line_t line) {
+    if (line.type) {} /* stop compiler complaints */
+    return (emit_t) { CODE, 0, {0} };
+}
+
+static char* inst_6502[] = {
+    "lda"
+};
+
+static emitter inst_x86[] = {
+    emit_lda
+};
 
 maybe(emit_t) emit(line_t line) {
     /* TODO: handle pseudo-ops somewhere */
     if (line.type == PSEUDO)
         return (maybe(emit_t)) none();
 
-    return (maybe(emit_t)) none(); /* TODO: implement */
+    for (size_t i = 0; i < sizeof(inst_6502); i++) {
+        if (strcmp(inst_6502[i], line.op) == 0)
+            return (maybe(emit_t)) some(inst_x86[i](line));
+    }
+
+    return (maybe(emit_t)) none();
 }
