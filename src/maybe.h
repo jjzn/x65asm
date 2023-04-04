59259730619012880476ba8c374a0367a2989c88
jjzn <jjzn@protonmail.com>
2023-04-04 10:42:43 +0200

    Make `unwrap` have consistent return types

--- src/maybe.h
+++ src/maybe.h
@@ -5,7 +5,7 @@
 
 #define make_maybe(T) struct maybe_##T { bool ok; T val; }
 #define maybe(T) struct maybe_##T
-#define unwrap(x) x.ok ? x.val : panic()
+#define unwrap(x) x.ok ? x.val : (panic("unwrapped none() value\n"), x.val)
 #define some(val) { true, val }
 #define none() { false }
 
