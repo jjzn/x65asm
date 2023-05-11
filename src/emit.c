cf8664be926ea743fb44ed56662f26f00b152a43
jjzn <jjzn@protonmail.com>
2023-05-11 14:24:38 +0200

    Implement store instructions (limited addressing)

--- src/emit.c
+++ src/emit.c
@@ -49,6 +49,31 @@ maybe(emit_t) _emit_load(line_t line, uint8_t reg) {
     return some(emit_t, res);
 }
 
+maybe(emit_t) _emit_store(line_t line, uint8_t reg) {
+    size_t len = 0;
+    uint8_t bin[MAX_BYTES_PER_INST];
+
+    switch (line.arg.type) {
+        case ZP:
+        case ABS:
+            len = 0;
+
+            bin[0] = 0x88;
+            bin[1] = 0x06 | (reg << 3);
+            bin[2] = line.arg.as_16; /* little-endian encoding */
+            bin[3] = line.arg.as_16 >> 8;
+            break;
+
+        default:
+            return none(emit_t);
+    }
+
+    emit_t res = { CODE, len, {0} };
+    memcpy(res.value, bin, len);
+
+    return some(emit_t, res);
+}
+
 maybe(emit_t) emit_lda(line_t line) {
     allow_args(line, IMM, ZP, ABS);
 
@@ -70,12 +95,35 @@ maybe(emit_t) emit_ldy(line_t line) {
     return _emit_load(line, REG_CL);
 }
 
+maybe(emit_t) emit_sta(line_t line) {
+    allow_args(line, ZP, ABS);
+
+    debug("emitting --> '%s'\n", __func__);
+    return _emit_store(line, REG_AL);
+}
+
+maybe(emit_t) emit_stx(line_t line) {
+    allow_args(line, ZP, ABS);
+
+    debug("emitting --> '%s'\n", __func__);
+    return _emit_store(line, REG_BL);
+}
+
+maybe(emit_t) emit_sty(line_t line) {
+    allow_args(line, ZP, ABS);
+
+    debug("emitting --> '%s'\n", __func__);
+    return _emit_store(line, REG_CL);
+}
+
 static char* inst_6502[] = {
-    "lda", "ldx", "ldy"
+    "lda", "ldx", "ldy",
+    "sta", "stx", "sty"
 };
 
 static emitter inst_x86[] = {
-    emit_lda, emit_ldx, emit_ldy
+    emit_lda, emit_ldx, emit_ldy,
+    emit_sta, emit_stx, emit_sty
 };
 
 maybe(emit_t) emit(line_t line) {
