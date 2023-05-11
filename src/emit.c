e9b738241d086ec7e062f7abf8af9f7d6db09b86
jjzn <jjzn@protonmail.com>
2023-05-11 09:03:33 +0200

    Make emitter functions return a `maybe(emit_t)`
    The type signature of emitter functions now returns a `maybe(emit_t)` type.
    Emitter functions now check if the argument type of the instruction is allowed.

--- src/emit.c
+++ src/emit.c
@@ -5,12 +5,23 @@
 #include "maybe.h"
 #include "encoding.h"
 
-typedef emit_t (*emitter)(line_t);
+#define allow_args(line, ...) \
+    arg_type_t _allowed_args[] = { __VA_ARGS__ }; \
+    bool _allowed_ok = false; \
+    for (size_t i = 0; i < sizeof(_allowed_args) / sizeof(arg_type_t); i++) \
+        _allowed_ok = _allowed_ok || line.arg.type == _allowed_args[i]; \
+    if (!_allowed_ok) \
+        return none(emit_t);
 
-emit_t emit_lda(line_t line) {
+typedef maybe(emit_t) (*emitter)(line_t);
+
+maybe(emit_t) emit_lda(line_t line) {
+    allow_args(line, IMM, ZP, ABS, IDX, IND, IDX_IND, IND_IDX);
+
     uint8_t bin[] = { 0x8B, REG_AL };
 
-    return (emit_t) { CODE, sizeof(bin), bin };
+    emit_t res = { CODE, sizeof(bin), bin };
+    return some(emit_t, res);
 }
 
 static char* inst_6502[] = {
@@ -27,8 +38,9 @@ maybe(emit_t) emit(line_t line) {
         return none(emit_t);
 
     for (size_t i = 0; i < sizeof(inst_6502); i++) {
-        if (strcmp(inst_6502[i], line.op) == 0)
-            return some(emit_t, inst_x86[i](line));
+        if (strcmp(inst_6502[i], line.op) == 0) {
+            return inst_x86[i](line);
+        }
     }
 
     return none(emit_t);
